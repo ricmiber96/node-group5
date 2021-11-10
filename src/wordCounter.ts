0488f9885a27982e780f1aa9e1af1b1fd75565bd
MiltonRivas <mdrq99@gmail.com>
2021-11-10 20:09:13 +0000

    add funtion aCounter

--- src/wordCounter.ts
+++ src/wordCounter.ts
@@ -1,7 +1,16 @@
 const phrase1: string = "Pienso, luego programo.";
 const phrase2: string = "La vida es una aventura que tenemos el privilegio de disfrutar.";
 
-const wordCounter = (phrase :string) :number => {
+const aCounter = (phrase: string): number => {
+  let counter = 0;
+  for (let index = 0; index < phrase.length; index++) {
+    if (phrase[index] == 'a')
+      counter++;
+  }
+  return counter;
+}
+
+const wordCounter = (phrase: string): number => {
   const counter = 0;
   const phraseSplitted = phrase.split(" ");
 
@@ -10,3 +19,6 @@ const wordCounter = (phrase :string) :number => {
 
 console.log("El número de palabras es: " + wordCounter(phrase1));
 console.log("El número de palabras es: " + wordCounter(phrase2));
+
+console.log("El número de a es: " + aCounter(phrase1));
+console.log("El número de a es: " + aCounter(phrase2));
